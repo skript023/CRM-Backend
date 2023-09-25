84e297450ba3e513951ee52e214e4b7d2da26580
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-09-25 07:11:51 +0700

    add multer

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import multer from 'multer';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
     app.useGlobalPipes(new ValidationPipe());
+    app.use(multer())
     app.enableCors({
         origin: true,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
