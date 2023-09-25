4d95967d8c9fcaddc7db8c68b90269699a7b3b51
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-09-25 22:02:52 +0700

    change uploads folder

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,10 +22,12 @@ export class AuthService
         };
     }
 
+    /*
     @Interval(3600000)
     handleCronOffline() {
 
     }
+    */
 
     private async encrypt(text: string): Promise<string>
     {
